306464edde41b6bf4718c08656974e7b513dd86b
davoodwadi <62418145+davoodwadi@users.noreply.github.com>
2025-02-02 16:48:03 -0800

    modified:   jschat-next/app/api/chatdummy/route.js 	modified:   jschat-next/app/scroll/page.js
    fixing build error

--- jschat-next/app/scroll/page.js
+++ jschat-next/app/scroll/page.js
@@ -1,21 +1,4 @@
-
-
 export default async function Page() {
-  const data = await fetch('http://localhost:3000/api/chat')
-  // const json = await data.text()
-  // console.log('json', json, data.status)
-  const reader = data.body.getReader();
-const decoder = new TextDecoder();
-let result = '';
-
-while (true) {
-  const { done, value } = await reader.read();
-  if (done) break;
-  
-  const chunk = decoder.decode(value, { stream: true });
-  console.log('Received chunk:', chunk);
-  result += chunk; // Append the chunk to the result
-}
   return (
     <>
       <div className="overflow-auto w-[30vw] h-[30vh] border border-red-500">
