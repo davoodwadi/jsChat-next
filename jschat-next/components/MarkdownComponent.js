0fcdbb2f310ff344bf9ddaae4b4ce7a07ac7c417
davoodwadi <62418145+davoodwadi@users.noreply.github.com>
2025-05-10 16:53:02 -0700

    Changes to be committed: 	modified:   jschat-next/components/MarkdownComponent.js fix footnote links

--- jschat-next/components/MarkdownComponent.js
+++ jschat-next/components/MarkdownComponent.js
@@ -1,14 +1,14 @@
 import Markdown from "react-markdown";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
+// import remarkParse from "remark-parse";
+// import remarkRehype from "remark-rehype";
 import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
+// import rehypeRaw from "rehype-raw";
 import rehypeKatex from "rehype-katex";
-import rehypeMathjax from "rehype-mathjax";
-import rehypeStringify from "rehype-stringify";
+// import rehypeMathjax from "rehype-mathjax";
+// import rehypeStringify from "rehype-stringify";
 import remarkMath from "remark-math";
 import rehypeFormat from "rehype-format";
-import { visit } from "unist-util-visit";
+// import { visit } from "unist-util-visit";
 
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import {
@@ -23,54 +23,7 @@ import CopyText from "@/components/CopyTextComponent";
 // import "@/node_modules/github-markdown-css/github-markdown.css";
 import "@/styles/markdown.css";
 // const inter = Inter({ subsets: ["latin"] });
-const preprocessMarkdown = (text) => {
-  // return text;
-  let processedTexts = text.replace(
-    /<think>([\s\S]*?)<\/think>/g,
-    "\n\n<think>\n$1\n</think>\n\n"
-  );
-  // Replace \[ ... \] with $$ ... $$ for block math
-  // processedTexts = processedTexts.replace(
-  //   /\\\[(.*?)\\\]/gs,
-  //   (_, match) => `$$${match}$$`
-  // );
-  processedTexts = processedTexts.replace(/\\\[/g, "```math ");
-  processedTexts = processedTexts.replace(/\\\]/g, "```");
-  // Replace \( ... \) with $ ... $ for inline math
-  processedTexts = processedTexts.replace(
-    /\\\((.*?)\\\)/gs,
-    (_, match) => `$${match}$`
-  );
-  return processedTexts;
-};
-function extractThinKContent(text) {
-  const startTag = "<think>";
-  const endTag = "</think>";
 
-  const startIndex = text.indexOf(startTag);
-  // console.log(startIndex, "startIndex");
-
-  // If no <think> tag
-  if (startIndex === -1) return { think: null, content: text };
-
-  // Look for closing </think> tag
-  const endIndex = text.indexOf(endTag);
-  // console.log(endIndex, "endIndex");
-
-  let think;
-  let content;
-  if (endIndex !== -1) {
-    // Closing tag exists — extract just the content between
-    think = text.slice(startIndex + startTag.length, endIndex);
-    content = text.slice(endIndex + endTag.length);
-  } else {
-    // No closing tag
-    think = text.slice(startIndex + startTag.length);
-    content = "";
-  }
-
-  return { content, think };
-}
 export default function MarkdownComponent(props) {
   // console.log("props.children.length MD", props.children.length);
 
@@ -175,7 +128,7 @@ export default function MarkdownComponent(props) {
         ]}
         rehypePlugins={[
           rehypeKatex,
-          rehypeFormat,
+          // rehypeFormat,
           // rehypeStringify,
           // rehypeRaw
         ]}
@@ -183,13 +136,33 @@ export default function MarkdownComponent(props) {
         className={`markdown-body`}
         // skipHtml={true}
         components={{
+          sup(props) {
+            const { children, className, node, ...rest } = props;
+            // console.log("children", children);
+            return <sup style={{ marginLeft: "0.3em" }}>{children}</sup>;
+          },
+          a(props) {
+            const { children, className, node, ...rest } = props;
+            if (!(className === "data-footnote-backref")) {
+              if (!rest.href.includes("http")) {
+                return <a href={rest.href}>{children}</a>;
+              } else {
+                return (
+                  <a
+                    href={rest.href}
+                    target={"_blank"}
+                    rel={"noopener noreferrer"}
+                  >
+                    {children}
+                  </a>
+                );
+              }
+            }
+          },
           code(props) {
             const { children, className, node, ...rest } = props;
             const text = children;
-            // console.log("code children", children);
-            // console.log("code className", className);
-            // console.log("node", node)
-            // console.log("...rest", rest)
+
             const match = /language-(\w+)/.exec(className || "");
             if (match) {
               // set language
@@ -270,12 +243,13 @@ const addCitationsToContent = (content, groundingChunks, groundingSupports) => {
             title: chunk.web.title,
           });
         }
-        return `${chunkIndex + 1}`;
+        return `[^${chunkIndex + 1}]`;
       })
-      .join(", ");
+      .join("");
 
-    const citationTextBrackets = ` [${citationText}]`;
-    console.log("citationTextBrackets", citationTextBrackets);
+    // const citationTextBrackets = ` [${citationText}]`;
+    const citationTextBrackets = `${citationText}`;
+    // console.log("citationTextBrackets", citationTextBrackets);
 
     // Insert citations at the end of the segment
     const { text } = segment;
@@ -301,8 +275,57 @@ const addCitationsToContent = (content, groundingChunks, groundingSupports) => {
       // console.log("chunkIndex, ref", chunkIndex, ref);
       // console.log(`[^${chunkIndex + 1}]: ${ref.url} "${ref.title}"\n`);
       // result += `[^${chunkIndex + 1}]: "${ref.title}" ${ref.url}\n`;
-      result += `[${chunkIndex + 1}] [${ref.title}](${ref.url})\n\n`;
+      result += `[^${chunkIndex + 1}]: [${ref.title}](${ref.url})\n\n`;
     }
   }
   return result;
 };
+
+const preprocessMarkdown = (text) => {
+  // return text;
+  let processedTexts = text.replace(
+    /<think>([\s\S]*?)<\/think>/g,
+    "\n\n<think>\n$1\n</think>\n\n"
+  );
+  // Replace \[ ... \] with $$ ... $$ for block math
+  // processedTexts = processedTexts.replace(
+  //   /\\\[(.*?)\\\]/gs,
+  //   (_, match) => `$$${match}$$`
+  // );
+  processedTexts = processedTexts.replace(/\\\[/g, "```math ");
+  processedTexts = processedTexts.replace(/\\\]/g, "```");
+  // Replace \( ... \) with $ ... $ for inline math
+  processedTexts = processedTexts.replace(
+    /\\\((.*?)\\\)/gs,
+    (_, match) => `$${match}$`
+  );
+  return processedTexts;
+};
+function extractThinKContent(text) {
+  const startTag = "<think>";
+  const endTag = "</think>";
+
+  const startIndex = text.indexOf(startTag);
+  // console.log(startIndex, "startIndex");
+
+  // If no <think> tag
+  if (startIndex === -1) return { think: null, content: text };
+
+  // Look for closing </think> tag
+  const endIndex = text.indexOf(endTag);
+  // console.log(endIndex, "endIndex");
+
+  let think;
+  let content;
+  if (endIndex !== -1) {
+    // Closing tag exists — extract just the content between
+    think = text.slice(startIndex + startTag.length, endIndex);
+    content = text.slice(endIndex + endTag.length);
+  } else {
+    // No closing tag
+    think = text.slice(startIndex + startTag.length);
+    content = "";
+  }
+
+  return { content, think };
+}
