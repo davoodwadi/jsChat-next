52d30a9fc781fac0d9f28ea861ff83c387a461e9
davoodwadi <62418145+davoodwadi@users.noreply.github.com>
2025-10-24 10:08:38 -0700

    footer text-muted-foreground

--- jschat-next/components/layout/sidebar-provider-server.jsx
+++ jschat-next/components/layout/sidebar-provider-server.jsx
@@ -43,7 +43,7 @@ export async function SidebarProviderServer({ params, children }) {
           </div>
 
           {children}
-          <footer className="flex gap-2 p-6 pt-24 flex-wrap items-center justify-center mt-auto text-xs md:text-sm">
+          <footer className="flex text-muted-foreground gap-2 p-6 pt-24 flex-wrap items-center justify-center mt-auto text-xs md:text-sm">
             <p className="flex items-center gap-2">
               © {today.getFullYear()} Spreed.chat. All rights reserved.
             </p>
